33d26424c8ddc3aba6dc9b629e546f184c1ec805
Mingu Kim <mkim245@gmail.com>
2022-11-07 12:27:07 -0500

    code channeldetail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+  const [channelDetail, setChannelDetail]=useState(null);
+  const { id } = useParams();
+  console.log(channelDetail);
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+  }, [id])
 
+return (
+  <div>{id}</div>
+)
+}
 export default ChannelDetail
